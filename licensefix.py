7f92162674afbcebac674e59694eed971ec307d3
Jason <jason.dixon.email@gmail.com>
2015-10-31 07:12:43 +1300

    throw in a semaphore

--- licensefix.py
+++ licensefix.py
@@ -10,6 +10,7 @@ class Fix(object):
     """
     Fix license in maya file
     """
+    block = threading.Semaphore()
     def __init__(s, license):
         s.license = license
         s.listener = cmds.scriptJob(e=['SceneSaved', s.wait], ro=True)
@@ -41,26 +42,27 @@ class Fix(object):
         # if ext == ".mb":
         #     reg = r"license.+?(?P<val>\w+)"
         if reg and os.path.isfile(filename):
-            search = True
-            try:
-                with tempfile.TemporaryFile() as w:
-                    with open(filename, "rb") as r:
-                        for line in r:
-                            if search:
-                                match = re.search(reg, line)
-                                if match:
-                                    pos = match.span("val")
-                                    line = line[: pos[0]] + s.license + line[pos[1] :]
-                                    search = False
-                            w.write(line)
-                    if not search:
-                        w.seek(0)
-                        with open(filename, "wb") as f:
-                            for line in w:
-                                f.write(line)
-                        s.message("License changed to \"%s\" in %s" % (s.license, filename))
-            except:
-                s.message(traceback.format_exc())
+            with s.block:
+                search = True
+                try:
+                    with tempfile.TemporaryFile() as w:
+                        with open(filename, "rb") as r:
+                            for line in r:
+                                if search:
+                                    match = re.search(reg, line)
+                                    if match:
+                                        pos = match.span("val")
+                                        line = line[: pos[0]] + s.license + line[pos[1] :]
+                                        search = False
+                                w.write(line)
+                        if not search:
+                            w.seek(0)
+                            with open(filename, "wb") as f:
+                                for line in w:
+                                    f.write(line)
+                            s.message("License changed to \"%s\" in %s" % (s.license, filename))
+                except:
+                    s.message(traceback.format_exc())
 
 import maya.utils as utils
 import maya.cmds as cmds
