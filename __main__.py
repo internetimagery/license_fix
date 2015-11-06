26bd0bb273966b6809bd05b71ec994bd973e76f3
Jason <jason.dixon.email@gmail.com>
2015-11-07 01:42:41 +1300

    moved files around

---
+++ __main__.py
@@ -0,0 +1,10 @@
+# Run on a file and fix license
+
+from . import Fix
+import argparse
+parser = argparse.ArgumentParser(
+    description="Change license of maya file.")
+parser.add_argument("license", help="License name.", type=str)
+parser.add_argument("input", help="File for processing.", type=str)
+args = parser.parse_args()
+Fix(args.license).fixfile(args.input)
